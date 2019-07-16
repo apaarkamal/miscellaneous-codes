c2e7646063c200b3c39238e18d8bf45d8bb05b25
apaarkamal <apaarsaxena@gmail.com>
2019-07-16 18:03:33 +0530

    added codes

--- cover them all hackerblocks.cpp
+++ cover them all hackerblocks.cpp
@@ -35,9 +35,32 @@ void the_martian(){
 const int N=200005;
 
 const int mod = 1000000007;
-inline int add(int x, int y){ x += y; return x%mod;}
-inline int sub(int x, int y){ x -= y; x%=mod; x+=mod; return x%mod;}
-inline int mul(int x, int y){ return ( x * y) % mod;}
+int fact[N], invfact[N];
+inline int add(int x, int y){ x += y; if(x >= mod) x -= mod; return x;}
+inline int sub(int x, int y){ x -= y; if(x < 0) x += mod; return x;}
+inline int mul(int x, int y){ return (((int) x) * y) % mod;}
+inline int powr(int a, int b){
+    int x = 1 % mod;
+    while(b){
+        if(b & 1) x = mul(x, a);
+        a = mul(a, a);
+        b >>= 1;
+    }
+    return x;
+}
+inline int inv(int a){ return powr(a, mod - 2);}
+void pre(){
+    fact[0] = invfact[0] = 1;
+    for(int i = 1;i < N; i++) fact[i] = mul(i, fact[i - 1]);
+    invfact[N - 1] = inv(fact[N - 1]);
+    for(int i = N - 2; i >= 1; i--) invfact[i] = mul(invfact[i + 1], i + 1);
+    assert(invfact[1] == 1);
+}
+
+inline int C(int n, int k){
+    if(n < k || k < 0) return 0;
+    return mul(fact[n], mul(invfact[k], invfact[n - k]));
+}
 
 bool comp(pair<pair<int,int>,int> a,pair<pair<int,int>,int> b){
     return a.F.S<b.F.S;
